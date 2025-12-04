1a328abbe91a58290a231c27df3364ca717acbc2
swift200923 <amrita232324@gmail.com>
2025-12-04 22:21:55 +0530

    Update AppointmentModal.tsx

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -1,72 +1,228 @@
-"use client";
+'use client';
+import { FormFieldType } from '@/lib/Type';
+import { getAppointmentSchema } from '@/lib/validation';
+import CustomFormField from '../CustomFormField';
+import { Form } from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { createAppointment, updateAppointment } from '@/lib/actions/appointment.actions';
 
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Doctors } from '@/constants';
+import { Appointment } from '@/lib/types/appwrite.types';
 
-import AppointmentForm from "./forms/AppointmentForm";
+import Image from 'next/image';
+import { SelectItem } from '../ui/select';
 
-import "react-datepicker/dist/react-datepicker.css";
-import { Appointment } from "@/lib/types/appwrite.types";
+import SubmitButton from '../SubmitButton';
 
-type AppointmentModalProps = {
-  patientId: string;
-  userId: string;
-  appointment?: Appointment;
-  type: "schedule" | "cancel";
-  title: string;
-  description: string;
+type AppointmentFormValues = {
+  primaryPhysician: string;
+  schedule: Date;
+  reason?: string;
+  note?: string;
+  cancellationReason?: string;
 };
 
-export const AppointmentModal = ({
+const AppointmentForm = ({
   patientId,
   userId,
-  appointment,
   type,
-  title,
-  description,
-}: AppointmentModalProps) => {
-  const [open, setOpen] = useState(false);
+  appointment,
+  setOpen,
+}: {
+  patientId: string;
+  userId: string;
+  type: 'create' | 'cancel' | 'schedule';
+  appointment?: Appointment;
+  setOpen?: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const AppointmentFormValidation = getAppointmentSchema(type);
 
-  const isSchedule = type === "schedule";
+  const form = useForm<AppointmentFormValues>({
+    resolver: zodResolver(AppointmentFormValidation as any) as any,
+    defaultValues: {
+      primaryPhysician: '',
+      schedule: new Date(Date.now()),
+      reason: '',
+      note: '',
+      cancellationReason: '',
+    },
+  });
+
+  const onSubmit = async ({
+    primaryPhysician,
+    schedule,
+    reason,
+    note,
+    cancellationReason,
+  }: AppointmentFormValues) => {
+    setIsLoading(true);
+    let status;
+
+    switch (type) {
+      case 'schedule':
+        status = 'scheduled';
+        break;
+      case 'cancel':
+        status = 'cancelled';
+        break;
+      default:
+        status = 'pending';
+    }
+
+    try {
+      if (type === 'create' && patientId) {
+        const appointmentData = {
+          userId,
+          patient: patientId,
+          primaryPhysician: primaryPhysician,
+          schedule: new Date(schedule),
+          reason: reason,
+          note: note,
+          status: status as any, // Status
+        };
+
+        const newAppointment = await createAppointment(appointmentData);
+
+        if (newAppointment) {
+          form.reset();
+          router.push(
+            `/patients/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`
+          );
+        }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment?.$id!,
+          appointment: {
+            primaryPhysician: primaryPhysician,
+            schedule: new Date(schedule),
+            status: status as any, // Status
+            cancellationReason: cancellationReason,
+          },
+          type,
+        };
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+        if (updatedAppointment) {
+          setOpen && setOpen(false);
+          form.reset();
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  let buttonLabel;
+  switch (type) {
+    case 'cancel':
+      buttonLabel = 'Cancel Appointment';
+      break;
+    case 'schedule':
+      buttonLabel = 'Schedule Appointment';
+      break;
+    default:
+      buttonLabel = 'Submit Appointment';
+  }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button
-          variant="ghost"
-          className={`capitalize ${
-            isSchedule ? "text-green-500" : "text-red-500"
-          }`}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">New Appointment</h1>
+          <p className="text-dark-700">
+            Request a new appointment in 10 seconds.
+          </p>
+        </section>
+
+        {type !== 'cancel' && (
+          <>
+            <CustomFormField
+              fieldType={FormFieldType.SELECT}
+              control={form.control}
+              name="primaryPhysician"
+              label="Primary care physician"
+              placeholder="Select a physician"
+            >
+              {Doctors.map((doctor, i) => (
+                <SelectItem key={doctor.name + i} value={doctor.name}>
+                  <div className="flex cursor-pointer items-center gap-2">
+                    <Image
+                      src={doctor.image}
+                      width={32}
+                      height={32}
+                      alt="doctor"
+                      className="rounded-full border border-dark-500"
+                    />
+                    <p>{doctor.name}</p>
+                  </div>
+                </SelectItem>
+              ))}
+            </CustomFormField>
+
+            <CustomFormField
+              fieldType={FormFieldType.DATE_PICKER}
+              control={form.control}
+              name="schedule"
+              label="Expected appointment date"
+              showTimeSelect
+              dateFormat="MM/dd/yyyy  -  h:mm aa"
+            />
+
+            <div
+              className={`flex flex-col gap-6  ${
+                type === 'create' && 'xl:flex-row'
+              }`}
+            >
+              <CustomFormField
+                fieldType={FormFieldType.TEXTAREA}
+                control={form.control}
+                name="reason"
+                label="Appointment reason"
+                placeholder="Annual montly check-up"
+                disabled={type === 'schedule'}
+              />
+
+              <CustomFormField
+                fieldType={FormFieldType.TEXTAREA}
+                control={form.control}
+                name="note"
+                label="Comments/notes"
+                placeholder="Prefer afternoon appointments, if possible"
+                disabled={type === 'schedule'}
+              />
+            </div>
+          </>
+        )}
+
+        {type === 'cancel' && (
+          <CustomFormField
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="cancellationReason"
+            label="Reason for cancellation"
+            placeholder="Urgent meeting came up"
+          />
+        )}
+
+        <SubmitButton
+          isLoading={isLoading}
+          className={`${
+            type === 'cancel' ? 'shad-danger-btn' : 'shad-primary-btn'
+          } w-full`}
         >
-          {type}
-        </Button>
-      </DialogTrigger>
-
-      <DialogContent className="shad-dialog sm:max-w-md">
-        <DialogHeader className="mb-4 space-y-3">
-          <DialogTitle className="capitalize">{title}</DialogTitle>
-          <DialogDescription>{description}</DialogDescription>
-        </DialogHeader>
-
-        <AppointmentForm
-          userId={userId}
-          patientId={patientId}
-          type={type}
-          appointment={appointment}
-          setOpen={setOpen}
-        />
-      </DialogContent>
-    </Dialog>
+          {buttonLabel}
+        </SubmitButton>
+      </form>
+    </Form>
   );
 };
 
-export default AppointmentModal;
+export default AppointmentForm;
